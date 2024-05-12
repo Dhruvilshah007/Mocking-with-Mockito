3d9f864e402e40f1cef282927ac3b453d2898be9
Dhruvilshah007 <sdhruvil72@gmail.com>
2024-05-12 22:00:58 +0530

    Unit Testing-Mocking with Mockito

---
+++ src/test/java/com/luv2code/test/MockAnnotationsTest.java
@@ -0,0 +1,154 @@
+package com.luv2code.test;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes= MvcTestingExampleApplication.class)
+public class MockAnnotationsTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    CollegeStudent studentOne;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+//    @Mock
+//    private ApplicationDao applicationDao;
+
+//    @InjectMocks
+    //It only will inject dependencies annotated with @Mock or @Spy
+//    private ApplicationService applicationService;
+//
+
+
+    //Using Spring Boot mockbean,this is prefereed approach
+
+    //It helps for mocking and also adds bean to Application context
+    @MockBean
+    private ApplicationDao applicationDao;
+    @Autowired
+    private ApplicationService applicationService;
+
+    @BeforeEach
+    public void beforeEach() {
+        studentOne.setFirstname("Eric");
+        studentOne.setLastname("Roby");
+        studentOne.setEmailAddress("eric.roby@luv2code_school.com");
+        studentOne.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("When & Verify")
+    @Test
+    public void assertEqualsTestAddGrades() {
+
+        //Mokcing and telling what response dao should send when addGradeResultsForSingleClass method is called
+        when(applicationDao.addGradeResultsForSingleClass(
+                studentGrades.getMathGradeResults())).thenReturn(100.00);
+
+        assertEquals(100, applicationService.addGradeResultsForSingleClass(
+                studentOne.getStudentGrades().getMathGradeResults()));
+
+        //It verifies that DAO method is called
+        verify(applicationDao).addGradeResultsForSingleClass(studentGrades.getMathGradeResults());
+
+        //How many times method is called, during the given test
+        verify(applicationDao, times(1)).addGradeResultsForSingleClass(
+                studentGrades.getMathGradeResults());
+    }
+
+    @DisplayName("Find Gpa")
+    @Test
+    public void assertEqualsTestFindGpa() {
+        when(applicationDao.findGradePointAverage(studentGrades.getMathGradeResults()))
+                .thenReturn(88.31);
+        assertEquals(88.31, applicationService.findGradePointAverage(studentOne
+                .getStudentGrades().getMathGradeResults()));
+    }
+
+    @DisplayName("Not Null")
+    @Test
+    public void testAssertNotNull() {
+        when(applicationDao.checkNull(studentGrades.getMathGradeResults()))
+                .thenReturn(true);
+        assertNotNull(applicationService.checkNull(studentOne.getStudentGrades()
+                .getMathGradeResults()), "Object should not be null");
+    }
+
+    @DisplayName("Throw runtime error")
+    @Test
+    public void throwRuntimeError() {
+
+        //Here we are getting object from context which will be null
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+        doThrow(new RuntimeException()).when(applicationDao).checkNull(nullStudent);
+
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        verify(applicationDao, times(1)).checkNull(nullStudent);
+    }
+
+    @DisplayName("Multiple Stubbing")
+    @Test
+    public void stubbingConsecutiveCalls() {
+        CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+
+
+        //For the first call throw an exception and later return a String
+        when(applicationDao.checkNull(nullStudent))
+                .thenThrow(new RuntimeException())
+                .thenReturn("Do not throw exception second time");
+//                .thenThrow(new RuntimeException());
+
+        assertThrows(RuntimeException.class, () -> {
+            applicationService.checkNull(nullStudent);
+        });
+
+        assertEquals("Do not throw exception second time",
+                applicationService.checkNull(nullStudent));
+
+        assertEquals("Do not throw exception second time",
+                applicationService.checkNull(nullStudent));
+
+//        assertThrows(RuntimeException.class, () -> {
+//            applicationService.checkNull(nullStudent);
+//        });
+
+        verify(applicationDao, times(3)).checkNull(nullStudent);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
